52523f95a45ce11e40ffaa4f5627044ab981b89e
parkjunyoung <pjoon86@naver.com>
2017-05-13 10:17:10 +0900

    React Bootstrap

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar , Nav , NavItem , MenuItem ,NavDropdown} from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Menu = (props) => {
     return (
-        <div>
-            <Link to="/">홈으로</Link>
-            <Link to="/posts">Posts</Link>
-        </div>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <Link to="/">Nodejs</Link>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <LinkContainer to="/posts">
+                        <NavItem>Posts</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/mypage">
+                        <NavItem>Mypage</NavItem>
+                    </LinkContainer>
+                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <MenuItem>Another action</MenuItem>
+                        <MenuItem>Something else here</MenuItem>
+                        <MenuItem divider />
+                        <MenuItem>Separated link</MenuItem>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
