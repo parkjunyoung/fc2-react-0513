1449728ab0d2457830d094d579536c29b1f4b839
parkjunyoung <pjoon86@naver.com>
2017-05-13 11:07:16 +0900

    validation, state제어

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch  } from 'react-router-dom';
 import Home from '../routes/Home';
 import Posts from '../routes/Posts';
 import Menu from './Menu';
+import Mypage from './Mypage';
+import NotExist from './NotExist';
 
 class App extends Component {
     render() {
@@ -10,8 +12,12 @@ class App extends Component {
             <Router>
                 <div>
                     <Menu/>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/posts" component={Posts} />
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/posts" component={Posts} />
+                        <Route path="/mypage" component={Mypage} />
+                        <Route component={NotExist} />
+                    </Switch>
                 </div>
             </Router>
         );
---
+++ src/components/NotExist.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotExist = (props) => {
+    return (
+        <div>페이지가 존재하지 않습니다.</div>
+    );
+}
+
+export default NotExist;
